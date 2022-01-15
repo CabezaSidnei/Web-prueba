f773e10d5ee05bb69ab85b202c02c81b45a5eb68
Sidnei <scsidnei@hotmail.com>
2022-01-15 17:40:18 -0300

    Solucion final javascript nivel Junior (index4)

--- codigo.js
+++ codigo.js
@@ -1,75 +1,33 @@
- class Celular {
-     constructor(color,peso,rdp,rdc,ram){
-        this.color = color;
-        this.peso = peso;
-        this.rdp = rdp;
-        this.rdc = rdc;
-        this.ram = ram;
-        this.encendido = false;
-     }
-     botonEncendido(){
-        if (!this.encendido) {
-            alert("Encender");
-            this.encendido = true;
-        }else{
-            alert("Apagar");
-            this.encendido = false;
-        }
-     }
+const contenedor = document.querySelector(".flex-container");
+//De esta manera trabajamos la logica javascript en el boton"COMPRAR"
+const boton = document.querySelector(".send-button");
+let valorAntiguo = boton.value;
+boton.value = valorAntiguo.toUpperCase();
 
-     reiniciar(){
-        if (this.encendido) {
-            alert("Reiniciando...");
-        }else{
-            alert("El celular esta apagado, enciendalo primero.");
-        }
-     }
-
-     tomarFoto(){
-        alert(`Foto tomada con camara de ${this.rdc}`);
-     }
-
-     grabarVideo(){
-        alert(`Video grabado con video de: ${this.rdc}`)
-     }
-
-     mostrarInfo(){
-         return `
-         Color: <b>${this.color}</b><br>
-         Peso: <b>${this.peso}</b><br>
-         Resolucion pantalla: <b>${this.rdp}</b><br>
-         Resolucion camara: <b>${this.rdc}</b><br>
-         Memoria RAM: <b>${this.ram}</b><br><br>
-         `;
-     }
-}
-
-class CelularAltaGama extends Celular{
-    constructor(color,peso,rdp,rdc,ram,camaraLenta,reconFacial,extraCamara){
-        super(color,peso,rdp,rdc,ram);
-        this.camaraLenta = camaraLenta;
-        this.reconFacial = reconFacial;
-        this.extraCamara = extraCamara;
-    }
-    mostrarInfoAltaGama(){
-        return this.mostrarInfo() + `
-            Camara Lenta: <b>${this.camaraLenta}</b><br>
-            Reconocimiento facial: <b>${this.reconFacial}</b><br>
-            Extra camara: <b>${this.extraCamara}</b><br><br>
-            `;
-    }
+function crearLLave(nombre,modelo,precio){
+    img =`<img class ="llave-img" src ="llave.png">`;
+    nombre = `<h2>LLave: ${nombre}</h2>`;
+    modelo = `<h3>Modelo: ${modelo}</h3>`;
+    precio = `<p>Precio: <b>$${precio}</b></p>`; 
+    return [img,nombre,modelo,precio];
 }
  
-const cel = new Celular("azul","35gr","3500px","16mpx","1,2gb");
-const cel2 = new Celular("negro","50gr","400px","12mpx","1.8gb");
-const cel3 = new Celular("blanco","300gr","800px","18mpx","2gb");
-const cel4 = new CelularAltaGama("Rojo","50gr","600px","20mpx","5GB","SI","SI","15mpx");
+let documentFragment = document.createDocumentFragment();
 
-document.write(`
-${cel.mostrarInfo()}
-${cel2.mostrarInfo()}
-${cel3.mostrarInfo()}
-${cel4.mostrarInfoAltaGama()}`);
+for(i=1;i<=20;i++){
+    let modeloRandom = Math.round(Math.random()*1000);
+    let precioRandom = Math.round(Math.random()*10+30);
+    let llave = crearLLave(i, modeloRandom, precioRandom);
+    let div = document.createElement("DIV");
+    div.addEventListener("click",()=>{
+        document.querySelector(".key-data").value = modeloRandom
+    });
+    div.tabIndex = i;
+    div.classList.add(`item-${i}`,`flex-item`);
+    div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
+    documentFragment.appendChild(div);
+}
 
+contenedor.appendChild(documentFragment);
 
 
